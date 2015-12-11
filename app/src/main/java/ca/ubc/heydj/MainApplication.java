951742a02d41c5d434783b3b2c369b205b8cee63
Chris Li <chrisli@hotmail.ca>
2015-12-11 01:57:50 -0800

    Initial commit.

---
+++ app/src/main/java/ca/ubc/heydj/MainApplication.java
@@ -0,0 +1,14 @@
+package ca.ubc.heydj;
+
+import android.app.Application;
+
+/**
+ * Created by Chris Li on 12/8/2015.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
